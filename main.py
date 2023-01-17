8de114c5129026b7e42cce70fea68f5050ee60a2
Replit user <>
2023-01-17 16:36:24 +0000

    Implement user input

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import numpy as np
 import math
 import networkx as nx
+from tabulate import tabulate
+
+
+def user_input():
+    n = int(input("Enter the number of vertices: "))
+    g = []
+    for i in range(n):
+        str = input(f"Row {i + 1}: ")
+        arr = [int(s) for s in str.split(' ')]
+        if len(arr) != n:
+            raise ValueError("Input row size does not match matrix dimension. Exiting...")
+        g.append(arr)
+    M = np.array(g)
+    return M
 
 
 def transition_matrix(adj_matrix):
     """Create transition matrix from adjacency matrix"""
     # TODO: Check for strongly connectedness
-
+    
     T = adj_matrix.copy()
     T = T.astype(float)
     nrows, ncols = T.shape
@@ -35,7 +49,9 @@ def page_rank(transition_matrix, num_it=1000):
 
 def main():
     g1 = np.array([[0, 1, 1, 1], [1, 0, 0, 1], [0, 1, 0, 1], [1, 0, 0, 0]])
-    T = transition_matrix(g1)
+    G = user_input()
+    print("Adjacency Matrix:\n", G)
+    T = transition_matrix(G)
     V = page_rank(T)
     print("Final Rank:\n", V)
 
