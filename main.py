55089be0e2fc97b9fb8d63d5bac3a6106c58b927
Replit user <>
2023-01-20 02:34:49 +0000

    Final Revisions

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import numpy as np
-import math
-import networkx as nx
-from tabulate import tabulate
 
 
 def user_input():
+    """Handle user input"""
     n = int(input("Enter the number of vertices: "))
     g = []
     for i in range(n):
         str = input(f"Row {i + 1}: ")
         arr = [int(s) for s in str.split(' ')]
         if len(arr) != n:
-            raise ValueError("Input row size does not match matrix dimension. Exiting...")
+            raise ValueError(
+                "Input row size does not match matrix dimension. Exiting...")
         g.append(arr)
     M = np.array(g)
     return M
@@ -19,8 +18,7 @@ def user_input():
 
 def transition_matrix(adj_matrix):
     """Create transition matrix from adjacency matrix"""
-    # TODO: Check for strongly connectedness
-    
+
     T = adj_matrix.copy()
     T = T.astype(float)
     nrows, ncols = T.shape
@@ -34,23 +32,34 @@ def transition_matrix(adj_matrix):
 
 
 def page_rank(transition_matrix, num_it=1000):
-    T = transition_matrix.copy().T # Need to explain why transpose
-    n = T.shape[0]
-    print("Transition Matrix:\n", T)
+    """PageRank Algorithm approach 2: """
+    P = transition_matrix.copy().T
+    n = P.shape[0]
+    print("Transition Matrix P^T:\n", P)
 
     V = np.array([1 / n] * n)
-    print("Rank vector:\n", V)
+    print("Rank vector V:\n", V)
 
     for i in range(num_it):
-        V = np.matmul(T, V)
+        V = np.matmul(P, V)
 
     return V
 
 
 def main():
+
+    print(
+        "Instructions: input the adjacency matrix of a strongly connected graph (every vertex can be reached from other vertices). Self-loops and multiple edges are allowed."
+    )
+    print()
+    print("Sample:")
     g1 = np.array([[0, 1, 1, 1], [1, 0, 0, 1], [0, 1, 0, 1], [1, 0, 0, 0]])
+    print(g1)
+    print()
+
     G = user_input()
     print("Adjacency Matrix:\n", G)
+
     T = transition_matrix(G)
     V = page_rank(T)
     print("Final Rank:\n", V)
